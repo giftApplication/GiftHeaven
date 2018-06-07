97a4c6ef7336f64deec4deb3f9b1053b7d80a323
momna <mommna.naseer@gmail.com>
2018-06-08 00:55:31 +0500

    login and signup functionality revised

--- app/src/main/java/com/example/moon/giftheaven/views/fragments/login_Fragment.java
+++ app/src/main/java/com/example/moon/giftheaven/views/fragments/login_Fragment.java
@@ -17,7 +17,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.moon.giftheaven.R;
-import com.example.moon.giftheaven.views.activities.Gmail_User_Info;
+//import com.example.moon.giftheaven.views.activities.Gmail_User_Info;
+//import com.example.moon.giftheaven.views.activities.MyDB;
 import com.example.moon.giftheaven.views.activities.MyDB;
 import com.example.moon.giftheaven.views.activities.after_login_activity;
 import com.facebook.FacebookSdk;
@@ -30,17 +31,38 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.mukeshsolanki.sociallogin.facebook.FacebookHelper;
 import com.mukeshsolanki.sociallogin.facebook.FacebookListener;
 
-import com.example.moon.giftheaven.R;
-import com.example.moon.giftheaven.views.activities.after_login_activity;
+//import com.example.moon.giftheaven.R;
+//import com.example.moon.giftheaven.views.activities.after_login_activity;
 
-public class login_Fragment extends Fragment implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+public class login_Fragment extends Fragment implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener,FacebookListener{
+
+
+
+    FacebookHelper fb_helper;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        fb_helper.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode==SIGN_IN_CODE){
+            GoogleSignInResult result= Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+
 
     private GoogleApiClient googleApiClient;
-    Button login, f_login,g_login;
+
+    Button login, fb_login,g_login;
     EditText uname, upassword;
     MyDB myDB;
     SQLiteDatabase db;
     Cursor cursor;
+    //private SignInButton gmail_btn;
+
     public static final int SIGN_IN_CODE=777;
 
     public void login_Fragemnt()
@@ -56,6 +78,25 @@ public class login_Fragment extends Fragment implements View.OnClickListener, Go
         uname = root.findViewById(R.id.uname);
         upassword = root.findViewById(R.id.upassword);
         upassword.setHint("Password");
+
+        //........................................Login with facebook wala kaam..................................................
+
+        FacebookSdk.setApplicationId(getResources().getString(R.string.facebook_app_id));
+        FacebookSdk.sdkInitialize(getActivity());
+        fb_helper=new FacebookHelper(login_Fragment.this);
+
+        fb_login=(Button)root.findViewById(R.id.fb_login);
+        fb_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fb_helper.performSignIn(getActivity());
+
+            }
+        });
+
+
+
+
 
         //...........................................................................
 
@@ -91,9 +132,27 @@ public class login_Fragment extends Fragment implements View.OnClickListener, Go
 
         login = (Button)root.findViewById(R.id.login_btn);
         login.setOnClickListener(this);
-
-
         return root;
+    }
+
+
+
+    @Override
+    public void onFbSignInFail(String errorMessage) {
+        Toast.makeText(getActivity(),"Not LoggedIn with Facebook!"+errorMessage,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onFbSignInSuccess(String authToken, String userId) {
+        Toast.makeText(getActivity(),"Successfully Signed In! "+userId,Toast.LENGTH_SHORT).show();
+        Intent myIntent1 = new Intent(getActivity().getApplicationContext(), after_login_activity.class);
+        startActivity(myIntent1);
+    }
+
+    @Override
+    public void onFBSignOut() {
+        Toast.makeText(getActivity(),"Signed Out!",Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -129,40 +188,6 @@ public class login_Fragment extends Fragment implements View.OnClickListener, Go
 
 
     }
-
-    //..............................................................................
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode==SIGN_IN_CODE){
-            GoogleSignInResult result= Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
-    }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-        if(result.isSuccess())
-        {
-            goMainScreen();
-        }
-        else
-        {
-            Toast.makeText(getActivity(), R.string.not_log_in,Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void goMainScreen() {
-        Intent i=new Intent(getActivity(), after_login_activity.class);
-        i.addFlags(i.FLAG_ACTIVITY_CLEAR_TOP | i.FLAG_ACTIVITY_CLEAR_TASK | i.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(i);
-    }
-
     private boolean checkDataEntered() {
         if(isEmpty(upassword) || isEmpty(uname)) {
             if (isEmpty(uname))
@@ -173,11 +198,38 @@ public class login_Fragment extends Fragment implements View.OnClickListener, Go
         }
         else return true;
     }
-
     private boolean isEmpty(EditText t) {
         CharSequence text = t.getText().toString();
         return (TextUtils.isEmpty(text));
 
     }
-    //..............................................................................
+
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+
+
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if(result.isSuccess())
+        {
+            goMainScreen();
+        }
+        else
+        {
+            Toast.makeText(getActivity(), "Not LoggedIn with Gmail!",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void goMainScreen() {
+        Intent i=new Intent(getActivity(), after_login_activity.class);
+        i.addFlags(i.FLAG_ACTIVITY_CLEAR_TOP | i.FLAG_ACTIVITY_CLEAR_TASK | i.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
+
 }
