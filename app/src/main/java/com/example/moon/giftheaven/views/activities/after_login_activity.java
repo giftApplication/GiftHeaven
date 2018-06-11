a2f4c3b99b4d70f87a55ae2e92cc5bbb954ea9a3
momna <mommna.naseer@gmail.com>
2018-06-11 15:00:51 +0500

    feedback and logout in menu

--- app/src/main/java/com/example/moon/giftheaven/views/activities/after_login_activity.java
+++ app/src/main/java/com/example/moon/giftheaven/views/activities/after_login_activity.java
@@ -1,10 +1,12 @@
 package com.example.moon.giftheaven.views.activities;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -15,16 +17,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.moon.giftheaven.R;
 import com.example.moon.giftheaven.views.adapter.event_category_budget;
 import com.example.moon.giftheaven.views.fragments.LockViewpager;
 
+import static com.example.moon.giftheaven.views.fragments.FragmentEvent.p_bar;
+
 public class after_login_activity extends AppCompatActivity {
     event_category_budget adapter;
     static public LockViewpager pager1;
-
+    /*********************************/
+    Dialog dialogf;
     String title;
     Toolbar myToolbar;
     static public int check;
@@ -37,7 +43,8 @@ public class after_login_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.after_login_activity);
         check = 0;
-
+/**********/
+        dialogf = new Dialog(this);
         pager1 = findViewById(R.id.pager1);
         pager1.setSwipeable(false);
         // pager1.setEnabled(false);
@@ -105,15 +112,15 @@ public class after_login_activity extends AppCompatActivity {
                         pager1.setCurrentItem(0);
                         switch (item.getItemId()) {
                             case R.id.nav_event:
-                              //  main_nav.setItemTextColor(ColorStateList.valueOf(Integer.parseInt("#601a36")));
+                                //  main_nav.setItemTextColor(ColorStateList.valueOf(Integer.parseInt("#601a36")));
                                 //frag = new login_Fragment();
                                 title = (String) adapter.getPageTitle(0);
                                 pager1.setCurrentItem(0);
                                 break;
                             case R.id.nav_category:
                             case R.id.nav_budget:
-                              // main_nav.setItemTextColor(ColorStateList.valueOf(Color.GRAY));
-                               //main_nav.setItemIconTintList(ColorStateList.valueOf(Color.GRAY));
+                                // main_nav.setItemTextColor(ColorStateList.valueOf(Color.GRAY));
+                                //main_nav.setItemIconTintList(ColorStateList.valueOf(Color.GRAY));
                                 Toast.makeText(getApplicationContext(), "Select event first", Toast.LENGTH_LONG).show();
                                 break;
 
@@ -171,42 +178,55 @@ public class after_login_activity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
 
     }
-    public  boolean onOptionsItemSelected(MenuItem item){
-        int id=item.getItemId();
+    public  boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
         //menu item handling
 
-        if(id==R.id.feedback)
-        {
-            Toast.makeText(this, "FeedBack", Toast.LENGTH_SHORT).show();
+        if (id == R.id.feedback) {
+            dialogf.setContentView(R.layout.feedback);
+            dialogf.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+            dialogf.show();
+            Button btn_submit = dialogf.findViewById(R.id.btn_submit);
+            btn_submit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialogf.dismiss();
+
+                }
+
+            });
+
+
 
         }
-        if(id==R.id.rate)
-        {
-            Intent intent=new Intent(this,SmileyRating.class);
-           startActivity(intent);
+        if (id == R.id.rate) {
+           /* Intent intent=new Intent(this,SmileyRating.class);
+           startActivity(intent);*/
 
             try {
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market:// details?id=" + getPackageName())));
-            }
-            catch (ActivityNotFoundException e)
-           {
-               startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?=" + getPackageName())));
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?=" + getPackageName())));
 
             }
-           Toast.makeText(this, "Rate us", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Rate us", Toast.LENGTH_SHORT).show();
         }
-        if(id==R.id.share)
-        {
-            Intent myIntent= new Intent(Intent.ACTION_SEND);
+        if (id == R.id.share) {
+            Intent myIntent = new Intent(Intent.ACTION_SEND);
             myIntent.setType("text/plain");
-            String shareBody="Your Body here";
-            String shareSub="Your subject here";
-            myIntent.putExtra(Intent.EXTRA_SUBJECT,shareSub);
-            myIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
-            startActivity(Intent.createChooser(myIntent,"Share using"));
+            String shareBody = "Your Body here";
+            String shareSub = "Your subject here";
+            myIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+            myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+            startActivity(Intent.createChooser(myIntent, "Share using"));
 
 
             Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+
+        }
+        if (id == R.id.logout) {
+            Intent intent = new Intent(this, Activity1.class);
+            startActivity(intent);
 
         }
         return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/com/example/moon/giftheaven/views/fragments/FeedbackDialogue.java
@@ -0,0 +1,26 @@
+package com.example.moon.giftheaven.views.fragments;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.moon.giftheaven.R;
+
+/**
+ * Created by HP on 6/7/2018.
+ */
+
+public class FeedbackDialogue extends DialogFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.feedback, container, false);
+        return v;
+    }
+}
